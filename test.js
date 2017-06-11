03203cd075ac51db97d62932061efaeae6e649d3
pasciaks <sheldon@pasciak.com>
2017-06-11 02:31:33 +0000

    push to git

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
 var PuzzleMakerScript = require("puzzlemakerscript");
 
-    PuzzleMakerScript.init(9,9);
+    PuzzleMakerScript.init(8,8);
+    
+    PuzzleMakerScript.set(5,5,"T");
+    PuzzleMakerScript.set(5,4,"E");
+    PuzzleMakerScript.set(4,4,"S");
+    PuzzleMakerScript.set(3,3,"T");
+    
+    for (var i=0;i<8;i++){
+        PuzzleMakerScript.set(i,0,"*");
+        PuzzleMakerScript.set(i,7,"=");
+    }   
+    
+    for (var i=0;i<8;i++){
+        PuzzleMakerScript.set(0,i,"@");
+        PuzzleMakerScript.set(7,i,"^");
+    }     
     
     PuzzleMakerScript.show();
-    
-    PuzzleMakerScript.set(0,0,"s");
-    PuzzleMakerScript.set(8,7,"p");
-    
-    PuzzleMakerScript.show();
-
-
-
-    
