a9cd67d0dea73611c22a78847ac0e23bdd3b1d31
pasciaks <sheldon@pasciak.com>
2017-07-14 05:47:06 +0000

    just checking around modify tests to theorize!

--- test.js
+++ test.js
@@ -6,15 +6,15 @@ var PuzzleMakerScript = require("puzzlemakerscript");
 
 function example1() {
 
-    PuzzleMakerScript.init(15,15);
-        var wordList=["SMART","SIGN","CAT","DOG","PIZZA","CAR","TREE"];
-        var howManyTimes = 1;
-            for (var i=0;i<wordList.length;i++) {
-             (PuzzleMakerScript.addWord(wordList[i],howManyTimes,1,1));  //set directions
-             (PuzzleMakerScript.addWord(wordList[i],howManyTimes,-1,1));  
-             (PuzzleMakerScript.addWord(wordList[i],howManyTimes,1,-1));  
-             (PuzzleMakerScript.addWord(wordList[i],howManyTimes,-1,-1));  
-            }
+    PuzzleMakerScript.init(15, 15);
+    var wordList = ["SMART", "SIGN", "CAT", "DOG", "PIZZA", "CAR", "TREE"];
+    var howManyTimes = 1;
+    for (var i = 0; i < wordList.length; i++) {
+        (PuzzleMakerScript.addWord(wordList[i], howManyTimes, 1, 1)); //set directions
+        (PuzzleMakerScript.addWord(wordList[i], howManyTimes, -1, 1));
+        (PuzzleMakerScript.addWord(wordList[i], howManyTimes, 1, -1));
+        (PuzzleMakerScript.addWord(wordList[i], howManyTimes, -1, -1));
+    }
     PuzzleMakerScript.fillBlanks(" ");
     PuzzleMakerScript.show();
 }
@@ -22,55 +22,82 @@ function example1() {
 
 function example2() {
 
-    PuzzleMakerScript.emptyChar='.';
-    PuzzleMakerScript.init(15,15);
-    PuzzleMakerScript.reuseLetters=true;
+    PuzzleMakerScript.emptyChar = '.';
+    PuzzleMakerScript.init(15, 15);
+    PuzzleMakerScript.reuseLetters = true;
 
-        var wordList=["SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON","SHELDON","AMANDA","AMANDA","SHELDON"];
-        var howManyTimes = 1;
-            for (var i=0;i<wordList.length;i++) {
-                var results=PuzzleMakerScript.addWord(wordList[i],howManyTimes); //random directions
-                //console.log(results);
-            }
-            
-    PuzzleMakerScript.fillBlanks(" ");//PuzzleMakerScript.emptyChar);
+    var wordList = ["SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON", "SHELDON", "AMANDA", "AMANDA", "SHELDON"];
+    var howManyTimes = 1;
+    for (var i = 0; i < wordList.length; i++) {
+        var results = PuzzleMakerScript.addWord(wordList[i], howManyTimes); //random directions
+        //console.log(results);
+    }
+
+    PuzzleMakerScript.fillBlanks(" "); //PuzzleMakerScript.emptyChar);
     PuzzleMakerScript.show();
 }
 
 function example3() {
     
-    var hidden=[];
     
-    PuzzleMakerScript.emptyChar='.';
-    PuzzleMakerScript.init(35,35);   
-    
+
+    var hidden = [];
+
+    PuzzleMakerScript.emptyChar = ' ';
+    PuzzleMakerScript.init(18,18);
+
     //PuzzleMakerScript.addWord("sheldon",1); 
     //PuzzleMakerScript.show();
     
-    for (var i=0;i<9999;i++){
-    var objResult =  PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(),PuzzleMakerScript.randSY(),"SHELDON",true,true);
-    // var objResult =  PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(),PuzzleMakerScript.randSY(),"amanda",false);
-    // var objResult =  PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(),PuzzleMakerScript.randSY(),"1234567",false);
-     if (objResult.thePath.length>0) {//console.log(JSON.stringify(objResult));
-     hidden.push(objResult);
-     if (hidden.length>=47) break;
-         
-     }
+    var theword="LOSTWORDS.ORG";
+    
+    var percentageofboardfilled = .35;
+    
+    var howmany = Math.floor ( (PuzzleMakerScript.cols*PuzzleMakerScript.rows) / theword.length * percentageofboardfilled);
+    
+    console.log(howmany);
+    
+    //howmany=12; // ********* <------- overrides above
+
+    for (var i = 0; i < 9999; i++) {
+        //var objResult =  PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(),PuzzleMakerScript.randSY(),"abcdefghijklm",false,true); 
+        //var objResult =  PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(),PuzzleMakerScript.randSY(),"123456789",false,true,70);
+                                                                                                                // reuse  // change dir  // dirchange percent
+        var objResult = PuzzleMakerScript.createPath(PuzzleMakerScript.randSX(), PuzzleMakerScript.randSY(), theword, false, true, 0); 
+        if (objResult.thePath.length > 0) {
+            //console.log(JSON.stringify(objResult));
+            hidden.push(objResult);
+            PuzzleMakerScript.show();
+            if (hidden.length >= howmany) break;
+
+        }
     }
-    
-    
-   PuzzleMakerScript.show();
-   console.log(JSON.stringify(hidden.length));
-   
+
+
+    PuzzleMakerScript.show();
+    console.log(JSON.stringify(hidden.length));
+
 
 }
 
-//example1();
+example1();
 
-//example2();
+example2();
 
 example3();
 
 // wowo todo   hide every engish word in a big puzzle
 
-//todo verify no two paths in create come back the same
+// todo verify no two paths in create come back the same
+
+// todo create path using presets already built according to style. ie.  right turns only
+
+// _ _ U N D R _ _ _ _
+// _ _ O P I I _ _ _ _
+// _ _ R S R G _ _ _ _
+// _ _ A L A H _ _ _ _
+// _ _ _ _ _ T _ _ _ _
+
+// SPIRALAROUNDRIGHT
+
+// MAYBE ZIGZAG ONE BEND AT HALF WAY MARK
